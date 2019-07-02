6a704512832fc46bc13f3e44774a3d2ea12b85fc
naythan893@gmail.com <naythan893@gmail.com>
2019-07-02 16:50:29 -0500

    End of GET

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit,OnDestroy } from '@angular/core';
+import {  Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import  { PostsService } from '../posts.service';
 
@@ -10,22 +11,26 @@ import  { PostsService } from '../posts.service';
   styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent  implements  OnInit {
+export class PostListComponent  implements  OnInit, OnDestroy {
   // postlist=[
   //   {title: 'First post',content: 'First post\'s content'},
   //   {title: 'Second post',content: 'Second post\'s content'},
   //   {title: 'Third post',content: 'Thrid post\'s content'}
   // ];
 postlist: Post[]=[];
-
+private postsSub :  Subscription;
   constructor(public postsService: PostsService) {}
+
 
   ngOnInit()  {
     this.postlist=this.postsService.getPosts();
-    this.postsService.getPostUpdatedListener()
+    this.postsSub =  this.postsService.getPostUpdatedListener()
     .subscribe((posts:  Post[]) =>  {
       this.postlist=posts;
 
     } );
+  }
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
  }
